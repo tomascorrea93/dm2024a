e9c0de9011df2a630475f86d31ba87efcc027ea5
tomascorrea93 <tocorrea@itba.edu.ar>
2024-05-13 23:55:28 -0300

    Pusheo cambios pc escritorio

--- src/rpart/Exper101_PrimerModelo.R
+++ src/rpart/Exper101_PrimerModelo.R
@@ -21,10 +21,10 @@ modelo <- rpart(
         formula = "clase_ternaria ~ .",
         data = dtrain, # los datos donde voy a entrenar
         xval = 0,
-        cp = -0.3, # esto significa no limitar la complejidad de los splits
-        minsplit = 0, # minima cantidad de registros para que se haga el split
-        minbucket = 1, # tamaño minimo de una hoja
-        maxdepth = 3
+        cp = -0.25, # esto significa no limitar la complejidad de los splits
+        minsplit = 1000, # minima cantidad de registros para que se haga el split
+        minbucket = 250, # tamaño minimo de una hoja
+        maxdepth = 4
 ) # profundidad maxima del arbol
 
 
@@ -60,6 +60,6 @@ dir.create("./exp/KA2001")
 
 # solo los campos para Kaggle
 fwrite(dapply[, list(numero_de_cliente, Predicted)],
-        file = "./exp/KA2001/K101_001.csv",
+        file = "./exp/KA2001/K101_055.csv",
         sep = ","
 )
