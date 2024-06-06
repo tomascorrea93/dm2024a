68bbbf574de01c62b486c102721eac75fa1edb85
tomascorrea93 <tocorrea@itba.edu.ar>
2024-06-06 21:36:08 +0000

    subo modelo final 1

--- src/workflow-01/MODELOFINAL1_TC_601_workflow_semi.r
+++ src/workflow-01/MODELOFINAL1_TC_601_workflow_semi.r
@@ -13,8 +13,8 @@ envg <- env()
 
 envg$EXPENV <- list()
 envg$EXPENV$bucket_dir <- "~/buckets/b1"
-envg$EXPENV$exp_dir <- "~/buckets/b1/expw/"
-envg$EXPENV$wf_dir <- "~/buckets/b1/flow/"
+envg$EXPENV$exp_dir <- "~/buckets/b1/expwMODELOFINAL1TC/"
+envg$EXPENV$wf_dir <- "~/buckets/b1/flowMODELOFINAL1TC/"
 envg$EXPENV$repo_dir <- "~/dm2024a/"
 envg$EXPENV$datasets_dir <- "~/buckets/b1/datasets/"
 envg$EXPENV$arch_sem <- "mis_semillas.txt"
@@ -93,7 +93,7 @@ FEintra_base <- function( pinputexps )
   if( -1 == (param_local <- exp_init())$resultado ) return( 0 ) # linea fija
 
 
-  param_local$meta$script <- "/src/workflow-01/z531_FE_intrames.r"
+  param_local$meta$script <- "/src/workflow-01/z531_FE_intramesvfinal.r"
 
   param_local$semilla <- NULL  # no usa semilla, es deterministico
 
@@ -128,14 +128,14 @@ FEhist_base <- function( pinputexps)
 
   param_local$meta$script <- "/src/workflow-01/z551_FE_historia.r"
 
-  param_local$lag1 <- TRUE
-  param_local$lag2 <- FALSE # no me engraso con los lags de orden 2
+  param_local$lag1 <- FALSE
+  param_local$lag2 <- TRUE # no me engraso con los lags de orden 2
   param_local$lag3 <- FALSE # no me engraso con los lags de orden 3
 
   # no me engraso las manos con las tendencias
-  param_local$Tendencias1$run <- TRUE  # FALSE, no corre nada de lo que sigue
+  param_local$Tendencias1$run <- FALSE  # FALSE, no corre nada de lo que sigue
   param_local$Tendencias1$ventana <- 6
-  param_local$Tendencias1$tendencia <- TRUE
+  param_local$Tendencias1$tendencia <- FALSE
   param_local$Tendencias1$minimo <- FALSE
   param_local$Tendencias1$maximo <- FALSE
   param_local$Tendencias1$promedio <- FALSE
@@ -175,10 +175,10 @@ FErf_attributes_base <- function( pinputexps, ratio, desvio)
   # parametros para que LightGBM se comporte como Random Forest
   param_local$lgb_param <- list(
     # parametros que se pueden cambiar
-    num_iterations = 20,
-    num_leaves  = 16,
-    min_data_in_leaf = 1000,
-    feature_fraction_bynode  = 0.2,
+    num_iterations = 50,
+    num_leaves  = 50,
+    min_data_in_leaf = 200,
+    feature_fraction_bynode  = 0.1,
 
     # para que LightGBM emule Random Forest
     boosting = "rf",
@@ -286,18 +286,22 @@ TS_strategy_base9 <- function( pinputexps )
 
 
   param_local$future <- c(202109)
-  param_local$final_train <- c(202007, 202006, 202105, 202104,
-    202103, 202102, 202101, 202012, 202011, 202010, 202009)
+  param_local$final_train <- c(202107, 202106, 202105, 202104, 202103, 202102, 202101,
+                               202012, 202011, 202010, 202009, 202008, 202007, 202004, 202003, 
+                               202002, 202001, 201912, 201911, 201910, 201909, 201908, 
+                               201907, 201906, 201905)
 
 
-  param_local$train$training <- c(202105, 202104, 202103,
-     202102, 202101, 202012, 202011, 202010, 202009)
+  param_local$train$training <- c(202105, 202104, 202103, 202102, 202101,
+                                  202012, 202011, 202010, 202009, 202008, 202007, 202004, 202003, 
+                                  202002, 202001, 201912, 201911, 201910, 201909, 201908, 
+                                  201907, 201906, 201905, 201904, 201903)
   param_local$train$validation <- c(202106)
   param_local$train$testing <- c(202107)
 
   # Atencion  0.2  de  undersampling de la clase mayoritaria,  los CONTINUA
   # 1.0 significa NO undersampling
-  param_local$train$undersampling <- 0.2
+  param_local$train$undersampling <- 0.7
   param_local$train$clase_minoritaria <- c( "BAJA+1", "BAJA+2")
 
   return( exp_correr_script( param_local ) ) # linea fija
@@ -457,10 +461,10 @@ wf_semillerio9 <- function( pnombrewf )
   DT_incorporar_dataset_competencia2024()
   CA_catastrophe_base( metodo="MachineLearning")
   FEintra_base()
-  DR_drifting_base(metodo="rank_cero_fijo")
+  DR_drifting_base(metodo="ninguno")
   FEhist_base()
   FErf_attributes_base()
-  #CN_canaritos_asesinos_base(ratio=0.2, desvio=4.0)
+  CN_canaritos_asesinos_base(ratio=0.1, desvio=0)
 
   ts9 <- TS_strategy_base9()
   ht <- HT_tuning_base()
